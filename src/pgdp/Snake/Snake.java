324827877b4eef5a57eec1f8d39238e5b7f5915c
MohanR <MohanR@G02DEXN04964.g02.fujitsu.local>
2021-03-06 19:18:57 +0100

    update 1

--- src/pgdp/Snake/Snake.java
+++ src/pgdp/Snake/Snake.java
@@ -9,36 +9,50 @@ public class Snake {
 
 	SnakeHead head;
 	Queue<SnakeElement> body;
+	Apple apple;
 
 	public Snake() {
 		body = new ArrayDeque<>();
 		head = new SnakeHead(Color.cyan, new Point(0, 0));
+		apple = spawnApple();
 	}
 
 	public void walk() {
 
+		if (head.getPosition().equals(apple.getPosition())) {
+			body.add(new SnakeElement(Color.green, new Point(head.getPosition().x, head.getPosition().y)));
+			apple = spawnApple();
+		}
+		moveBody();
+		moveHead();
+
 	}
 
-	public void spawnApple() {
-
+	public Apple spawnApple() {
+		return null;
 	}
-	
+
 	private void moveHead() {
-		
-		head.getPosition().x++;
-		
-		
+
+		head.getPosition().x += Math.sin(Math.toRadians(head.getDirection()));
+		head.getPosition().y += Math.sin(Math.toRadians(head.getDirection()));
+
 	}
-	
+
 	private void moveBody() {
-		
+
+		body.add(new SnakeElement(Color.green, new Point(head.getPosition().x, head.getPosition().y)));
+		body.remove();
+
 	}
-	
+
 	private void turnTo(double direction) {
-		
+
+		if (head.getDirection() + 180 == direction || head.getDirection() - 180 == direction) {
+			return;
+		} else
+			head.setDirection(direction);
 	}
-	
-	
 
 }
 
